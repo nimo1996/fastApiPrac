4628fc81fb3978d5483692a62f76a83c24a354b2
nimo1996 <nimo196@gmail.com>
2024-05-13 18:59:42 +0900

    first commit

---
+++ api/schemas/task.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, Field
+
+class TaskBase(BaseModel):
+    title: str | None = Field(None, example="세탁소에 맡긴 것을 찾으러 가기")
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskCreateResponse(TaskCreate):
+    id: int
+    
+    class Config:
+        orm_mode = True
+
+class Task(TaskBase):
+    id: int
+    done: bool = Field(False, description="완료 플래그")
+
+    class Config:
+        orm_mode = True
+
+
